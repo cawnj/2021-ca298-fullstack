1978d23dbd0c09c3a1b0120a9cb816fe5d513399
Conor Joyce <conor.joyce27@mail.dcu.ie>
2021-03-13 20:44:18 +0000

    added user model

--- ca298/app/models.py
+++ ca298/app/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
 
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
 
 class Order(models.Model):
     id = models.AutoField(primary_key=True)
@@ -15,3 +18,7 @@ class Order(models.Model):
 
     def price(self):
         return self.product.price * self.quantity
+
+
+class CaUser(AbstractUser):
+    is_admin = models.BooleanField(default=False)
