4ad163493342388753c3cbd9d7817767807be0f4
Conor Joyce <conor.joyce27@mail.dcu.ie>
2021-03-23 21:59:05 +0000

    removed django_extensions dependency

--- ca298/ca298/settings.py
+++ ca298/ca298/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
-    'django_extensions',
-    'django.contrib.sitemaps',
+    #'django_extensions',
+    #'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
